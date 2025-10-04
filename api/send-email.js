10c4f28121cdccb76799609d16e3365a8570ecef
TCollins2001 <tc486@njit.edu>
2025-10-04 15:22:56 -0400

    :wq

--- api/send-email.js
+++ api/send-email.js
@@ -2,6 +2,8 @@ import { Resend } from 'resend';
 
 const resend = new Resend(process.env.RESEND_API_KEY);
 
+console.log("Resend key:", process.env.RESEND_API_KEY);
+
 export default async function handler(req, res) {
   if (req.method !== 'POST') {
     return res.status(405).json({ error: 'Method Not Allowed' });
